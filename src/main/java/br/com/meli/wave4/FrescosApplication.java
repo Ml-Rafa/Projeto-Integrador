8ac28f8feaf74929fcf8067f0afdb508dc495e80
afaeoliveira <rafael.aoliveira@mercadolivre.com>
2022-01-26 18:10:38 -0300

    commit inicial

---
+++ src/main/java/br/com/meli/wave4/FrescosApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.wave4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrescosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrescosApplication.class, args);
+    }
+
+}
