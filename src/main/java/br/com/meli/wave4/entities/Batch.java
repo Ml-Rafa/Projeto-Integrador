423f2a499d67cf73679bc613e6ce651a3933e855
Paula Rodrigues da Silva <paula.prodrigues@mercadolivre.com>
2022-01-27 12:11:15 -0300

    Adicionado relacionamento Batch - Product

--- src/main/java/br/com/meli/wave4/entities/Batch.java
+++ src/main/java/br/com/meli/wave4/entities/Batch.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -22,7 +19,11 @@ public class Batch {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer batchNumber;
-    private Integer productId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
     private Double currentTemperature;
     private Double minimumTemperature;
     private Integer initialQuantity;
