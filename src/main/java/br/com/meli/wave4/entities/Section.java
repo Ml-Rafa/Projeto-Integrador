07fa01a28dccba188768a08579e49807b63f0f35
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2022-01-27 09:47:15 -0300

    add attributes and anotations to Section class

--- src/main/java/br/com/meli/wave4/entities/Section.java
+++ src/main/java/br/com/meli/wave4/entities/Section.java
@@ -1,4 +1,24 @@
 package br.com.meli.wave4.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Section {
+
+    @Id
+    Integer sectionCode;
+    Double temperature;
+    Double maxCapacity;
+    Double availableCapacity;
+
 }
