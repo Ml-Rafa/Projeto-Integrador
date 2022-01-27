01663f88c5594a98ab275a420020f15e27453b2a
Paula Rodrigues da Silva <paula.prodrigues@mercadolivre.com>
2022-01-27 17:37:35 -0300

    Adicionado método verifyWarehouse

--- src/main/java/br/com/meli/wave4/services/InboundOrderService.java
+++ src/main/java/br/com/meli/wave4/services/InboundOrderService.java
@@ -1,7 +1,25 @@
 package br.com.meli.wave4.services;
 
+import br.com.meli.wave4.entities.Warehouse;
+import br.com.meli.wave4.repositories.WarehouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InboundOrderService {
+
+    @Autowired
+    WarehouseRepository warehouseRepository;
+
+    public boolean verifyWarehouse(Warehouse warehouse) {
+
+        Warehouse warehouseExists = this.warehouseRepository.getById(warehouse.getId());
+
+        Optional<Warehouse> byId = this.warehouseRepository.findById(warehouse.getId());
+
+        return true;
+    }
+
 }
