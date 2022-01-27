a1856d2d9696470da09abf52d11b3d42c658aac1
gabielarocha21 <gabriela.rocha@mercadolivre.com>
2022-01-27 09:48:38 -0300

    Adição de userEntity e userRepository

--- src/main/java/br/com/meli/wave4/entities/User.java
+++ src/main/java/br/com/meli/wave4/entities/User.java
@@ -1,4 +1,26 @@
 package br.com.meli.wave4.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String sobrenome;
+    private String cpf;
+
 }
