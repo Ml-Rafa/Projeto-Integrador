6780a9bc25bc4ee43eecde3ba24b41c331cac4fe
afaeoliveira <rafael.aoliveira@mercadolivre.com>
2022-02-04 11:12:28 -0300

    teste RepresentativeServiceTest findById

--- src/main/java/br/com/meli/wave4/entities/Representative.java
+++ src/main/java/br/com/meli/wave4/entities/Representative.java
@@ -1,9 +1,6 @@
 package br.com.meli.wave4.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
 public class Representative extends User {
 
     @Id
---
+++ src/test/java/br/com/meli/wave4/unit_tests/RepresentativeServiceTest.java
@@ -0,0 +1,59 @@
+package br.com.meli.wave4.unit_tests;
+
+
+import br.com.meli.wave4.entities.Batch;
+import br.com.meli.wave4.entities.Representative;
+import br.com.meli.wave4.entities.Warehouse;
+import br.com.meli.wave4.repositories.RepresentativeRepository;
+import br.com.meli.wave4.services.RepresentativeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepresentativeServiceTest {
+
+    @InjectMocks
+    RepresentativeService representativeService;
+
+    @Mock
+    RepresentativeRepository representativeRepository;
+
+    Representative representative;
+
+    Warehouse warehouse;
+
+    List<Batch> batchList = new ArrayList<>();
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        this.warehouse =
+                Warehouse.builder().build();
+
+        this.representative =
+                new Representative();
+                this.representative.setId(3);
+                this.representative.setWarehouse(this.warehouse);
+                this.representative.setBatch(this.batchList);
+    }
+
+    @Test
+    public void findById(){
+
+        when(this.representativeRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(this.representative));
+        assertEquals(3,this.representativeService.findById(any()).getId());
+
+
+    }
+
+
+}
