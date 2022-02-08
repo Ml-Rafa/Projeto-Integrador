139f3a5ffe2938bfb073cb65016ec492ea786d92
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2022-02-08 12:58:32 -0300

    Add PurchaseOrderService unit test

--- src/test/java/br/com/meli/wave4/unit_tests/RepresentativeServiceTest.java
+++ src/test/java/br/com/meli/wave4/unit_tests/RepresentativeServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.any;
