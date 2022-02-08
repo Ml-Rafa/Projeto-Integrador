2ffd65d0fb55b325d53e23e6761bbec6e0818346
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2022-02-08 09:35:16 -0300

    Add UserServiceTest unit test

---
+++ src/test/java/br/com/meli/wave4/unit_tests/UserServiceTest.java
@@ -0,0 +1,42 @@
+package br.com.meli.wave4.unit_tests;
+
+import br.com.meli.wave4.DTO.BatchDTO;
+import br.com.meli.wave4.DTO.UserDTO;
+import br.com.meli.wave4.entities.Batch;
+import br.com.meli.wave4.entities.User;
+import br.com.meli.wave4.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.Arrays;
+
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    UserDTO user;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        user = UserDTO
+                .builder()
+                .id(10)
+                .name("Maria")
+                .document("44444")
+                .address("Rua 123")
+                .telephone("222222")
+                .build();
+    }
+
+    @Test
+    public void convertToEntity(){
+        assertInstanceOf(User.class, this.userService.convertToEntity(this.user));
+    }
+
+}
