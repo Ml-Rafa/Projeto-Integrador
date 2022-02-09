9fd6266159634527b3cc34b0d7ed87fc6ac516f5
gabielarocha21 <gabriela.rocha@mercadolivre.com>
2022-02-09 17:36:27 -0300

    teste client

---
+++ src/test/java/br/com/meli/wave4/unit_tests/clientServiceTest.java
@@ -0,0 +1,25 @@
+package br.com.meli.wave4.unit_tests;
+
+
+import br.com.meli.wave4.entities.*;
+import br.com.meli.wave4.repositories.UserRepository;
+import br.com.meli.wave4.services.ClientService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class clientServiceTest {
+
+}
